6ba8ffb6efe95f86ecde1ed89340d7d915e4b0a3
ptcharliechen <128341777+ptcharliechen@users.noreply.github.com>
2026-03-20 12:53:16 +0800

    Update split.py

--- split.py
+++ split.py
@@ -1,42 +1,43 @@
+from kit.args import args
+arg = args({"output": "split", "incar": [None, str, "INCAR path"], "kpoints": [None, str, "KPOINTS path"], "potcar": [None, str, "POTCAR path"]}, \
+        "POSCAR", steps=False, output_isdir=True)
 from os import path, mkdir
 from shutil import copy
+from copy import deepcopy
 from kit.fundamental import Atom
 from kit.vasp import POSCAR
-from kit.interface import args, line
+from kit.interface import line
 
 if __name__ == "__main__":
-    arg = args({"output": "split", "incar": None, "kpoints": None, "potcar": None}, "POSCAR")
     ads_poscar = POSCAR(arg)
     print("\nAdsorbate:")
-    ads_atomList = line(ads_poscar)
-    ads_poscar.read_atom(ads_atomList)
+    ads_atoms = line(ads_poscar)
+    ads_poscar.read_all(ads_atoms)
     while(1):
         inp = input("\nDo you want to use the remaining as bulk atoms (y/n) [y]: ")
         if inp == "":
             inp = 'y'
         if inp.lower() == 'n':
-            print("Bulk:\n")
+            print("Bulk:")
             bulk_poscar = POSCAR(arg)
-            bulk_atomList = line(bulk_poscar)
-            bulk_poscar.read_atom(bulk_atomList)
+            bulk_atoms = line(bulk_poscar)
+            bulk_poscar.read_all(bulk_atoms)
             break
         elif inp.lower() == 'y':
-            bulk_atomList = Atom()
-            bulk_atomList.bridge = ads_poscar
-            tmp = ads_atomList.get()
+            bulk_atoms = deepcopy(Atom(ads_poscar))
+            ads_atom_list = ads_atoms.get()
             for atom in range(len(ads_poscar.elements)):
-                if atom not in tmp:
-                    bulk_atomList.put(atom)
+                if atom not in ads_atom_list:
+                    bulk_atoms.put(atom)
             bulk_poscar = POSCAR(arg)
-            bulk_poscar.read_atom(bulk_atomList)
+            bulk_poscar.read_all(bulk_atoms)
             break
     total_poscar = POSCAR(arg)
-    total_atomList = Atom()
-    total_atomList.start = total_poscar.start
-    total_atomList.put(ads_atomList.get())
-    total_atomList.put(bulk_atomList.get())
-    total_atomList.sort()
-    total_poscar.read_atom(total_atomList)
+    total_atoms = deepcopy(Atom(arg))
+    total_atoms.put(ads_atoms.get())
+    total_atoms.put(bulk_atoms.get())
+    total_atoms.sort()
+    total_poscar.read_all(total_atoms)
     for kind in ["incar", "kpoints", "potcar"]:
         if getattr(arg.args, kind) is None:
             continue
@@ -76,19 +77,19 @@ if __name__ == "__main__":
 
     mkdir(arg.args.output)
     elements = []
-    for poscar, atomList, kind in zip([bulk_poscar, ads_poscar, total_poscar], [bulk_atomList, ads_atomList, total_atomList], ["bulk", "adsorbate", "total"]):
+    for poscar, atoms, kind in zip([bulk_poscar, ads_poscar, total_poscar], [bulk_atoms, ads_atoms, total_atoms], ["bulk", "adsorbate", "total"]):
         mkdir(path.join(arg.args.output, kind))
         write_poscar = POSCAR(arg)
         write_poscar.args.output = path.join(arg.args.output, kind, "POSCAR")
         write_poscar.lattice = poscar.lattice
         element = []
-        tmp = atomList.get()
+        tmp = atoms.get()
         for idx, _ in enumerate(poscar.elements):
             if idx in tmp:
                 element.append(_)
         elements.append(element)
         write_poscar.elements = element
-        write_poscar.atom_list = atomList
+        write_poscar.atoms = atoms
         write_poscar.fractional_position = poscar.fractional_position
         write_poscar.write_all()
 
