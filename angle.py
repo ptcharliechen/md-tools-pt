f913c7ecfeb8fdd10103630040a6a900156805b3
ptcharliechen <128341777+ptcharliechen@users.noreply.github.com>
2026-03-20 12:39:57 +0800

    Update angle.py

--- angle.py
+++ angle.py
@@ -1,27 +1,25 @@
+from kit.args import args
+arg = args({"output": "", "radian": [False, "use radian as output unit"]}, "XDATCAR", elements=False)
 from os import getcwd
 from kit.vasp import XDATCAR
-from kit.interface import args, single, wrap, smooth
+from kit.interface import single, wrap, smooth
 from kit.etc import write_csv
 
 if __name__ == "__main__":
-    arg = args({"output": None, "atomfileside": [None, str, "provide the side atoms information. default is None"], "radian": [False, "use radian as output unit"]}, "XDATCAR")
     xdatcar = XDATCAR(arg)
     
-    atoms = single(xdatcar, 1, "Center atom")
-    xdatcar.args.atomfile = arg.args.atomfileside
-    atoms.put(single(xdatcar, 2, "Side atoms"))
-    xdatcar.args = arg.args
+    atoms = single(xdatcar, 1, line=[1], name="Center atom")
+    atoms.put(single(xdatcar, 2, line=[2], show_info_flag=False, name="Side atoms"))
     
     xdatcar.read_all(atoms=atoms)
-    dist = wrap(xdatcar, type="a")
+    angle = wrap(xdatcar, type="a")
     
-    line, factor = smooth(dist)
+    line, factor = smooth(angle)
     
     atoms = atoms.get(True)
     cen_atom, side_atom_1, side_atom_2 = atoms[0], atoms[1], atoms[2]
     
-    filename = ("{}_{}_{}".format(cen_atom, side_atom_1, side_atom_2) if arg.args.output is None else arg.args.output)
-    arg.same_name(getcwd(), "{}.csv".format(filename), factor)
-    
-    write_csv(filename, line, tuple(range(xdatcar.final_step)), "{}_{}_{}".format(cen_atom, side_atom_1, side_atom_2))
+    filename = (f"{cen_atom}_{side_atom_1}_{side_atom_2}" if arg.args.output == "" else arg.args.output)
+    filename = arg.same_name(getcwd(), filename+".csv", factor)
+    write_csv(filename, line, tuple(range(xdatcar.final_step)), f"{cen_atom}_{side_atom_1}_{side_atom_2}")
     print("Done!")
