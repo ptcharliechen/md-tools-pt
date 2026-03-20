57dc806f31a8500838210a37d3377bf7f160e50e
ptcharliechen <128341777+ptcharliechen@users.noreply.github.com>
2026-03-20 12:55:47 +0800

    Update v2g.py

--- v2g.py
+++ v2g.py
@@ -1,16 +1,15 @@
+from kit.args import args
+arg = args({"output": "poscar"}, "gjf", steps=False, atoms=False)
 from os import getcwd
 from kit.vasp import POSCAR
 from kit.software import Gaussian
-from kit.interface import args
 
 if __name__ == "__main__":
-    arg = args({"output": "POSCAR.gjf"}, "gjf")
     poscar = POSCAR(arg)
     poscar.read_all()
-    gaussian = Gaussian(arg)
-    gaussian.bridge = poscar
+    gaussian = Gaussian(poscar)
     gaussian.args.output = poscar.title
-    arg.same_name(getcwd(), "{}.gjf".format(gaussian.args.output))
+    arg.same_name(getcwd(), f"{gaussian.args.output}.gjf")
     gaussian.cartesian_position = poscar.cartesian_position
     gaussian.write_all()
     print("Done!")
