07544f56f2ae0cf74f10fbb1a4997274bf0c3a90
ptcharliechen <128341777+ptcharliechen@users.noreply.github.com>
2026-03-20 12:56:12 +0800

    Update wrap.py

--- wrap.py
+++ wrap.py
@@ -1,26 +1,25 @@
+from kit.args import args
+arg = args({"output": ""}, "XDATCAR", elements=False)
 from os import getcwd
 from kit.vasp import XDATCAR
-from kit.interface import args, single, wrap, smooth
+from kit.interface import single, wrap, smooth
 from kit.etc import write_csv
 
 if __name__ == "__main__":
-    arg = args({"output": None, "atomfile2": [None, str, "provide the second atom information. default is None"]}, "XDATCAR")
     xdatcar = XDATCAR(arg)
     
-    atom_list = single(xdatcar, 1, name="Atom 1")
-    xdatcar.args.atomfile = arg.args.atomfile2
-    atom_list.put(single(xdatcar, 1, name="Atom 2"))
-    
-    xdatcar.args = arg.args
+    atom_list = single(xdatcar, 1, line=[1], name="Central atom")
+    atom_list.put(single(xdatcar, 1, line=[2], show_info_flag=False, name="Measured atom"))
     
     xdatcar.read_all(atoms=atom_list)
     
     dist = wrap(xdatcar, type="d")
-    
-    line, factor = smooth(dist)
+
+    line, factor = smooth(dist[0] if dist.ndim == 2 else dist[0][0].T)
+    line = line.T if line.ndim == 2 else line
     atom_list = atom_list.get(True)
     cen_atom, mea_atom = atom_list[0], atom_list[1]
-    filename = (f"{cen_atom} {mea_atom}" if arg.args.output is None else arg.args.output)
-    filename = arg.same_name(getcwd(), f"{filename}.csv", factor)
-    write_csv(filename, line, tuple(range(xdatcar.final_step)), f"{cen_atom}_{mea_atom}")
+    filename = (f"{cen_atom}_{mea_atom}" if arg.args.output == "" else arg.args.output)
+    filename = arg.same_name(getcwd(), filename+".csv", factor)
+    write_csv(filename, line if line.ndim < 3 else line[0][0], tuple(range(xdatcar.final_step)), f"{cen_atom}_{mea_atom}")
     print("Done!")
