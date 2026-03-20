777b7bef4e0b1300eded5605ba51a3e031706b75
ptcharliechen <128341777+ptcharliechen@users.noreply.github.com>
2026-03-20 12:42:08 +0800

    Update direction.py

--- direction.py
+++ direction.py
@@ -1,14 +1,15 @@
+from kit.args import args
+arg = args({"output": "direction"}, "XDATCAR")
 from kit.vasp import XDATCAR
 from kit.accelerate import image_shift
-from kit.interface import args, line, smooth
+from kit.interface import line, smooth
 from kit.etc import write_csv
 
 if __name__ == "__main__":
-    arg = args({"output": "Direction"}, "XDATCAR")
     xdatcar = XDATCAR(arg)
     
     atom_list = line(xdatcar)
-    atom_list.sort()
+
     while(1):
         cart_dir = input("Which direction you would like to discover (x/y/z) [z]: ")
         if cart_dir.lower() == 'x' or cart_dir.lower() == 'y' or cart_dir.lower() == 'z':
@@ -18,8 +19,9 @@ if __name__ == "__main__":
             break
         else:
             print("Warning: Input 'x', 'y', or 'z'.")
-    print("\nWait a moment!")
+
     xdatcar.read_all(atoms=atom_list)
+
     direction = image_shift(xdatcar.fractional_position, xdatcar.lattice)
     if cart_dir.lower() == 'x':
         pos = direction[:, :, 0]
@@ -28,8 +30,9 @@ if __name__ == "__main__":
     elif cart_dir.lower() == 'z':
         pos = direction[:, :, 2]
     
-    lines, factor = smooth(pos)
+    lines, factor = smooth(pos.T)
     if factor:
         arg.args.output += f"_Alpha={factor}"
     write_csv(arg.args.output, lines, list(range(pos.shape[0])), atom_list.get(True))
     print("Done!")
+
