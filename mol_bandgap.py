78fac438065ea1251ba38105b2121c04618e59a2
ptcharliechen <128341777+ptcharliechen@users.noreply.github.com>
2026-03-20 12:48:20 +0800

    Update mol_bandgap.py

--- mol_bandgap.py
+++ mol_bandgap.py
@@ -1,15 +1,16 @@
+from kit.args import args
+arg = args({"output": "DOS", "fermi": [1, int, "fermi level correction. default is open (i.e. 1)"], \
+            "threshold": [0.0002, float, "provide the DOS threshold"]}, "vasprun.xml", molecules=True, output_isdir=True)
 from os import mkdir, path
 from numpy import array, zeros
 from kit.fundamental import Molecule
 from kit.vasp import POSCAR
-from kit.interface import args, lines
+from kit.interface import lines
 
 if __name__ == "__main__":
-    arg = args({"output": "DOS", "fermi": [1, int, "fermi level correction. default is open (i.e. 1)"], "molecules": [None, str, "specify a file with molecule information"], "threshold": [0.0002, float]}, "vasprun.xml")
-    
     poscar = POSCAR(arg)
     poscar.args.input = "POSCAR"
-    atom_list = lines(poscar)
+    atom_list, output_info = lines(poscar, print_output_flag=True)
     
     orbital_order = ["s", "py", "pz", "px", "dxy", "dyz", "dz2", "dxz", "dx2-y2"]
     while(1):
@@ -60,30 +61,41 @@ if __name__ == "__main__":
     atoms_states = array(atoms_states)
 
     with open(path.join(arg.args.output, "list.csv"), "w") as write_file:
-        write_file.write("No.,molecule,HOMO 1,HOMO 2,LUMO 1,LUMO 2,Gap,Step,Atoms\n")
-    for mol in atom_list:
+        write_file.write("No.,molecule,HOMO,LUMO,Gap,Step,Atoms\n")
+    not_mol_count = 0
+    for idx, mol in enumerate(atom_list):
         atoms = mol.get()
         for key, molecule_list in molecules.molecule_dictionary.items():
-            if atoms[0] in molecule_list:
+            if atoms[0] in molecule_list and len(atoms) == len(molecule_list):
+                with open(path.join(arg.args.output, "list.csv"), "a") as list_file:
+                    list_file.write(f"{key+1},{molecules.molecule_kind[key]},")
                 break
+        else:
+            key = -1
+            not_mol_count += 1
+            with open(path.join(arg.args.output, "list.csv"), "a") as list_file:
+                list_file.write("0,Not a molecule,")
         DOS_orbitals = zeros((atoms_states.shape[1], len(orbitals)))
         DOS_total = zeros((atoms_states.shape[1], 9))
         for i in atoms:
-            for idx, j in enumerate(orbitals):
-                DOS_orbitals[:, idx] += atoms_states[i, :, j]
+            for idx_2, j in enumerate(orbitals):
+                DOS_orbitals[:, idx_2] += atoms_states[i, :, j]
             DOS_total += atoms_states[i]
-        with open(path.join(arg.args.output, molecules.molecule_kind[key]+".csv"), "w") as write_file:
-            HOMO = zeros(2)
-            LUMO = zeros(2)
+        filename = molecules.molecule_kind[key] if key != -1 else str(not_mol_count)
+        with open(path.join(arg.args.output, filename+".csv"), "w") as write_file:
+            HOMO = 0.0
+            LUMO = 0.0
             LUMO_flag = False
             for i in range(atoms_states.shape[1]):
                 write_file.write(f"{atoms_eigens[key][i]:.4f},{sum(DOS_orbitals[i]):.4f}\n")
-                if i > 0 and not LUMO_flag:
-                    if sum(DOS_total[i]) < arg.args.threshold and sum(DOS_total[i-1]) > arg.args.threshold and atoms_eigens[key][i] < 0:
-                        HOMO[0], HOMO[1] = atoms_eigens[key][i-1], atoms_eigens[key][i]
-                    elif sum(DOS_total[i]) > arg.args.threshold and sum(DOS_total[i-1]) < arg.args.threshold and atoms_eigens[key][i] > 0:
-                        LUMO[0], LUMO[1] = atoms_eigens[key][i-1], atoms_eigens[key][i]
+                if i > 1 and not LUMO_flag:
+                    if sum(DOS_total[i]) < arg.args.threshold and sum(DOS_total[i-1]) >= arg.args.threshold and atoms_eigens[key][i] < 0:
+                        HOMO = atoms_eigens[key][i-1]
+                    elif sum(DOS_total[i]) >= arg.args.threshold and sum(DOS_total[i-1]) < arg.args.threshold and atoms_eigens[key][i] > 0:
+                        LUMO = atoms_eigens[key][i]
                         LUMO_flag = True
         with open(path.join(arg.args.output, "list.csv"), "a") as list_file:
-            list_file.write(f"{key+1},{molecules.molecule_kind[key]},{HOMO[0]:.4f},{HOMO[1]:.4f},{LUMO[0]:.4f},{LUMO[1]:.4f},{LUMO[1]-HOMO[0]:.4f},{LUMO[1]-LUMO[0]:.4f},{str(molecules.molecule_dictionary[key]).replace(',', ' ')}\n")
+            list_file.write(f"{HOMO:.4f},{LUMO:.4f},{LUMO-HOMO:.4f},{atoms_eigens[key][i]-atoms_eigens[key][i-1]:.4f},")
+            list_file.write(" ".join(info for info in output_info[idx])+'\n')
     print("Done!")
+
