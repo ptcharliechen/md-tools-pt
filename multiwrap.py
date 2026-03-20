f67cdc45e6f982e0eaed92a7f2d352073c9cefa8
ptcharliechen <128341777+ptcharliechen@users.noreply.github.com>
2026-03-20 12:50:15 +0800

    Update multiwrap.py

--- multiwrap.py
+++ multiwrap.py
@@ -1,10 +1,12 @@
+from kit.args import args
+arg = args({"output": "distances"}, "XDATCAR")
 from os import getcwd
-from numpy import array, newaxis
 from copy import deepcopy
+import numpy as np
 from kit.fundamental import AtomStep_Trj, Atom
 from kit.vasp import XDATCAR
 from kit.function import Distance
-from kit.interface import couple, args, smooth
+from kit.interface import couple, smooth
 from kit.etc import write_csv
 
 class Atom_Repeat(Atom):
@@ -14,11 +16,10 @@ class Atom_Repeat(Atom):
         self._atom_list.append(atom)
 
 if __name__ == "__main__":
-    arg = args({"output": "distances"}, "XDATCAR")
     xdatcar = XDATCAR(arg)
     
     atom_lists, total_lists = couple(xdatcar, 2, atom=Atom_Repeat(), Atom_split_flag=True, flatten_flag=True)
-    
+
     xdatcar.read_all(atoms=total_lists)
     pos_lists = []
     frac_pos = xdatcar.fractional_position
@@ -29,22 +30,17 @@ if __name__ == "__main__":
             pos.append(frac_pos[:, total_lists.index(atom.get()[0])])
         pos_lists.append(deepcopy(pos))
     
-    distance = Distance(xdatcar)
-    cen_atom_step, mea_atom_step = AtomStep_Trj(xdatcar), AtomStep_Trj(xdatcar)
-    center_positions, measure_positions = [], []
-    center_atoms, measure_atoms = Atom_Repeat(xdatcar), Atom_Repeat(xdatcar)
+    distances = []
     for atom_list, pos_list in zip(atom_lists, pos_lists):
-        center_atoms.put(atom_list[0])
-        measure_atoms.put(atom_list[1])
-        center_positions.append(pos_list[0])
-        measure_positions.append(pos_list[1])
-    center_positions, measure_positions = array(center_positions).transpose((1, 0, 2)), array(measure_positions).transpose((1, 0, 2))
-    cen_atom_step.steps, cen_atom_step.atoms, cen_atom_step.fractional_position = xdatcar.steps, center_atoms, center_positions
-    mea_atom_step.steps, mea_atom_step.atoms, mea_atom_step.fractional_position = xdatcar.steps, measure_atoms, measure_positions
-    distance.center_atom_step, distance.measure_atom_step = cen_atom_step, mea_atom_step
-    distance.auto()
-    distances_arr = distance.distance()
-    lines, factor = smooth(distances_arr)
+        distance = Distance(xdatcar)
+        cen_atom_step, mea_atom_step = AtomStep_Trj(xdatcar), AtomStep_Trj(xdatcar)
+        cen_atom_step.steps, cen_atom_step.atoms, cen_atom_step.fractional_position = xdatcar.steps, atom_list[0], pos_list[0][np.newaxis, :].transpose((1, 0, 2))
+        mea_atom_step.steps, mea_atom_step.atoms, mea_atom_step.fractional_position = xdatcar.steps, atom_list[1], pos_list[1][np.newaxis, :].transpose((1, 0, 2))
+        distance.center_atom_step, distance.measure_atom_step = cen_atom_step, mea_atom_step
+        distance.auto()
+        distances.append(distance.distance())
+    
+    lines, factor = smooth(np.array(distances)[:, 0, :])
     arg.args.output = arg.same_name(getcwd(), arg.args.output, factor)
     columns = [f"{atom_list[0].get(True)[0]} {atom_list[1].get(True)[0]}" for atom_list in atom_lists]
     write_csv(arg.args.output, lines, xdatcar.steps.get(True), columns)
