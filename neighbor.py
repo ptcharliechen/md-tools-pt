9855ce8a4bd24ed2de8e270b62dc3b892181d141
ptcharliechen <128341777+ptcharliechen@users.noreply.github.com>
2026-03-20 12:50:50 +0800

    Update neighbor.py

--- neighbor.py
+++ neighbor.py
@@ -1,72 +1,74 @@
+from kit.args import args
+#Preprocess
+arg = args({"output": ["neighbors", "output file name includes atom numbers of every molecule"], \
+            "bond": [None, str, "specify a file with user-defined bond length threshold"], \
+            "mode": ["t", str, "type mode (t), or molecule mode (m)"]}, "POSCAR", molecules=True)
 from numpy import where
 from collections import defaultdict
+from re import match
 from kit.vasp import POSCAR
-from kit.interface import line, args
-from kit.function import Ligand
-from molecule import threshold
+from kit.interface import line, read_bond
+from kit.fundamental import Graph
 
 if __name__ == "__main__":
-    #Preprocess
-    arg = args({"output": ["neighbors", "output file name includes atom numbers of every molecule"], \
-                "bond": [None, str, "specify a file with user-defined bond length threshold"], \
-                "molecules": [None, str, "molecule information file"]}, "POSCAR")
     #Get atoms
     poscar = POSCAR(arg)
     
     atoms = line(poscar)
     poscar.read_all(atoms=atoms)
-    molecule = Ligand()
-    molecule.bridge = poscar
+    molecule = Graph(poscar)
+
     #Set the bond threshold
-    if arg.args.bond is not None:
-        molecule.read_bond()
-    else:
-        bondType = threshold()
-        if bondType is not None:
-            molecule.threshold = bondType
-    molecule.build_molecule_position()
+    read_bond(poscar)
+    poscar.read_molecules()
     molecule.build_graph()
     graph = molecule.adjacent_matrix
-    poscar.read_molecules()
-    MoleKinds, moleDict = poscar.atom_step.molecules.molecule_kind, poscar.atom_step.molecules.molecule_dictionary
-    AtomKind, MoleculeElements = {}, defaultdict(list)
+    
+    if arg.args.mode.lower() == "m":
+        mole_kinds, mole_dict = poscar.atom_step.molecules.molecule_kind, poscar.atom_step.molecules.molecule_dictionary
+    else:
+        mole_kinds, mole_dict = [_.split("_")[0] for _ in poscar.atom_step.molecules.molecule_kind], poscar.atom_step.molecules.molecule_dictionary
+    atom_kind, molecule_elements = {}, defaultdict(list)
+    
     ElementsList = list(set(molecule.elements))
-    for num in sorted(moleDict.keys()):
-        for atom in moleDict[num]:
+    for num in sorted(mole_dict.keys()):
+        for atom in mole_dict[num]:
             surrondingElements = defaultdict(int)
-            for _ in where(graph[atom])[0]:
+            for _ in where(graph[molecule.atoms.index_list[atom]])[0]:
                 if _ == atom:
                     continue
                 surrondingElements[molecule.elements[_]] += 1
                 element = molecule.elements[_]
-            IdxList = poscar.atom_step.index_list
-            AtomKind[atom] = [MoleKinds[num], molecule.elements[IdxList[atom]], str(dict(surrondingElements))]
-            if molecule.elements[IdxList[atom]] not in MoleculeElements[MoleKinds[num]]:
-                MoleculeElements[MoleKinds[num]].append(molecule.elements[IdxList[atom]])
-    
+            IdxList = poscar.atom_step.atoms.index_list
+            atom_kind[atom] = [mole_kinds[num], molecule.elements[IdxList[atom]], str(dict(surrondingElements))]
+            if molecule.elements[IdxList[atom]] not in molecule_elements[mole_kinds[num]]:
+                molecule_elements[mole_kinds[num]].append(molecule.elements[IdxList[atom]])
+
     with open(arg.args.output+".csv", "w") as write_file:
         recorded = []
-        for MoleKind in set(MoleKinds):
-            write_file.write(f"Molecule: {MoleKind}\n")
-            for element in MoleculeElements[MoleKind]:
+        mole_kinds_sorted = sorted(set(mole_kinds)) if arg.args.mode.lower() == "t" else sorted(set(mole_kinds), key=lambda x: (match(r'(\D*)(\d+)', x).groups()[0], int(match(r'(\D*)(\d+)', x).groups()[1])))
+        for mole_kind in mole_kinds_sorted:
+            write_file.write(f"Molecule: {mole_kind}\n")
+            for element in molecule_elements[mole_kind]:
                 write_file.write(f"Element: {element}\n")
-                for AtomI, ValI in AtomKind.items():
-                    if AtomI in recorded:
+                for atom_i, val_i in atom_kind.items():
+                    if atom_i in recorded:
                         continue
-                    if ValI[0] == MoleKind and ValI[1] == element:
-                        recorded.append(AtomI)
-                        if ValI[2] == "{}":
-                            write_file.write(f"None:\n{AtomI}\n")
+                    if val_i[0] == mole_kind and val_i[1] == element:
+                        recorded.append(atom_i)
+                        if val_i[2] == "{}":
+                            write_file.write(f"None,total: 1 atoms\n{atom_i}\n")
                         else:
                             atoms = []
-                            write_file.write(f"{ValI[2]}:\n")
-                            atoms.append(int(AtomI))
-                            for AtomJ, ValJ in AtomKind.items():
-                                if AtomJ in recorded:
+                            write_file.write(val_i[2].replace('{','').replace('}','').replace("'","")+",")
+                            atoms.append(int(atom_i))
+                            for atom_j, val_j in atom_kind.items():
+                                if atom_j in recorded:
                                     continue
-                                if ValJ[0] == MoleKind and ValJ[1] == element and ValJ[2] == ValI[2]:
-                                    atoms.append(int(AtomJ))
-                                    recorded.append(AtomJ)
+                                if val_j[0] == mole_kind and val_j[1] == element and val_j[2] == val_i[2]:
+                                    atoms.append(int(atom_j))
+                                    recorded.append(atom_j)
+                            write_file.write(f"total: {len(atoms)} atoms\n")
                             for idx, i in enumerate(sorted(atoms)):
                                 if idx == 0:
                                     write_file.write(f"{i}")
@@ -77,3 +79,4 @@ if __name__ == "__main__":
                         continue
             write_file.write("\n")
     print("Done!")
+
